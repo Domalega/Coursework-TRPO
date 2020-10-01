2509f518d8ddc4b6a459519de723d844d318a644
Domalega <vladimir.domalega@gmail.com>
2020-10-01 15:16:46 +0700

    Add changes in ThirdTest.hpp

---
+++ src/ThirdTest.hpp
@@ -0,0 +1,7 @@
+#ifndef THIRDTEST_H
+#define THIRDTEST_H
+#include "Arrayes.hpp"
+
+int ThirdTest(string* ListI[], string* ListV2[], string* ListV3[], string* ListT[], int* listShuffle[]);
+
+#endif
