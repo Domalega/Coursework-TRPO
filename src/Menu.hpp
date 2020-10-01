a9cd26f79ae82bb72adee88dd7c32fc1e110fe7f
Domalega <vladimir.domalega@gmail.com>
2020-10-01 23:11:21 +0700

    Fix in Menu.hpp

--- src/Menu.hpp
+++ src/Menu.hpp
@@ -1,7 +1,6 @@
 #ifndef MENU_H
 #define MENU_H
-#include "Arrayes.hpp"
 
-void Menu(string* ListI[], string* ListV2[], string* ListV3[], string* ListT[], int* listShuffle[]);
+void Menu();
 
 #endif
