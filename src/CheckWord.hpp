dc5d3ade249081f16c129ffe78236db53d55120f
Domalega <vladimir.domalega@gmail.com>
2020-10-01 15:12:11 +0700

    Add changes in CheckWord.hpp

---
+++ src/CheckWord.hpp
@@ -0,0 +1,8 @@
+#ifndef CHECKWORD_H
+#define CHECKWORD_H
+#include "Arrayes.hpp"
+
+int CheckWord(string* ListI[], string* ListV2[], string* ListV3[], string* ListT[], int* listShuffle[]);
+
+#endif
+
