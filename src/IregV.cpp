abbf0782c745c983a914af84e164b353204be5a8
Domalega <vladimir.domalega@gmail.com>
2020-10-01 23:10:58 +0700

    Fix in IregV.cpp

--- src/IregV.cpp
+++ src/IregV.cpp
@@ -1,17 +1,10 @@
-#include <iostream>
-#include <string>
-#include <cstdlib>
-#include <iostream>
 #include <locale.h>
-#include <ctime>
 #include "Menu.hpp"
-#include "Arrayes.hpp"
 
 using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "Russian");
-	srand(time(0));
-	Menu(*ListI, string* ListV2[], string* ListV3[], string* ListT[], int* listShuffle[]);
+	Menu();
 	return 0;
 }
