c7336192125819c6e58b7ed502b51a0af3a7b808
Domalega <vladimir.domalega@gmail.com>
2020-10-01 15:11:24 +0700

    Add changes in Arrayes.hpp

---
+++ src/Arrayes.hpp
@@ -0,0 +1,15 @@
+#ifndef ARRAYES_H
+#define ARRAYES_H
+
+#include <string>
+
+using namespace std;
+
+string ListI[120];
+string ListV2[120];
+string ListV3[120];
+string ListT[120];
+
+int listShuffle[120];
+
+#endif
