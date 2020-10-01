7793b3d3aaa58277c293066ff81073ce09c607f1
Domalega <vladimir.domalega@gmail.com>
2020-10-01 23:16:35 +0700

    Fix in TestWord.hpp

--- src/TestWord.hpp
+++ src/TestWord.hpp
@@ -1,7 +1,6 @@
 #ifndef TESTWORD_H
 #define TESTWORD_H
-#include "Arrayes.hpp"
 
-int TestWord(string* ListI[], string* ListV2[], string* ListV3[], string* ListT[], int* listShuffle[]);
+int TestWord();
 
 #endif
