fd904d7a0236d784f4cb8afd9272a57028fc6644
Domalega <vladimir.domalega@gmail.com>
2020-10-01 15:15:24 +0700

    Add changes in SecondTest.hpp

---
+++ src/SecondTest.hpp
@@ -0,0 +1,8 @@
+#ifndef SECONDTEST_H
+#define SECONDTEST_H
+#include "Arrayes.hpp"
+#include "TestWord.hpp"
+
+int SecondTest(string* ListI[], string* ListV2[], string* ListV3[], string* ListT[], int* listShuffle[]);
+
+#endif
