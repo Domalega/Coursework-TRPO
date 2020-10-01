8122bca8493f52114b63d6138abd2a5d38171093
Domalega <vladimir.domalega@gmail.com>
2020-10-01 23:09:59 +0700

    Fix in CheckWord.hpp

--- src/CheckWord.hpp
+++ src/CheckWord.hpp
@@ -1,8 +1,7 @@
 #ifndef CHECKWORD_H
 #define CHECKWORD_H
-#include "Arrayes.hpp"
 
-int CheckWord(string* ListI[], string* ListV2[], string* ListV3[], string* ListT[], int* listShuffle[]);
+int CheckWord();
 
 #endif
 
