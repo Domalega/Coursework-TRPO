8cca69f9d5af6458051cd0a1d6508766800adb22
Domalega <vladimir.domalega@gmail.com>
2020-10-01 23:11:53 +0700

    Fix in SecondTest.hpp

--- src/SecondTest.hpp
+++ src/SecondTest.hpp
@@ -1,8 +1,7 @@
 #ifndef SECONDTEST_H
 #define SECONDTEST_H
-#include "Arrayes.hpp"
 #include "TestWord.hpp"
 
-int SecondTest(string* ListI[], string* ListV2[], string* ListV3[], string* ListT[], int* listShuffle[]);
+int SecondTest();
 
 #endif
