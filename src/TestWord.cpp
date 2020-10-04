4e5fa44fdb268c891eda349e8593b2817c9aacde
MrGanris <ammosov2001@gmail.com>
2020-10-04 23:43:41 +0700

    New Fix in TestWord.cpp

--- src/TestWord.cpp
+++ src/TestWord.cpp
@@ -8,31 +8,43 @@
 
 using namespace std;
 
-int TestWord() {
+int TestWord()
+{
 	string Test;
-	while (true) {
+	while (true)
+	{
 		cout << "Выберите уровень сложности проверки слов: ";
-		cout << endl << "1. Легкий (5 слов)" << endl << "2. Средний (10 слов)" << endl << "3. Сложный (20 слов)" << endl << "4. Выйти в меню" << endl << "5. Выход из программы" << endl;
+		cout << endl
+			 << "1. Легкий (5 слов)" << endl
+			 << "2. Средний (10 слов)" << endl
+			 << "3. Сложный (20 слов)" << endl
+			 << "4. Выйти в меню" << endl
+			 << "5. Выход из программы" << endl;
 		cout << "Слова вводятся нижним регистром через ентер каждый отдельно." << endl;
 		cin >> Test;
-		if (Test == "1") {
+		if (Test == "1")
+		{
 			FirstTest();
 			system("pause");
 			system("cls");
 		}
-		if (Test == "2") {
+		if (Test == "2")
+		{
 			SecondTest();
 			system("cls");
 		}
-		if (Test == "3") {
+		if (Test == "3")
+		{
 			ThirdTest();
 			system("cls");
 		}
-		if (Test == "4") {
+		if (Test == "4")
+		{
 			system("cls");
 			Menu();
 		}
-		if (Test == "5") {
+		if (Test == "5")
+		{
 			system("cls");
 			cout << "Выходим из программы..." << endl;
 			exit(0);
