b3acd8dad5534dda97c292ce40dcd9bb780e780c
Jeff <XJefferson@rambler.ru>
2020-10-05 19:06:15 +0700

    Add new library

---
+++ src/WinLose.hpp
@@ -0,0 +1,17 @@
+#ifndef WINLOSE_H
+#define WINLOSE_H
+
+#include <string>
+
+using namespace std;
+
+void WinLose(
+        string* V2,
+        string* V3,
+        string* List1,
+        string* List2,
+        int* win,
+        int* lose,
+        int* count);
+
+#endif
