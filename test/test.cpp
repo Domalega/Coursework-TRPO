3deda210e24f5991eac20999bfed78107befd6f3
Jeff <XJefferson@rambler.ru>
2020-10-05 19:14:34 +0700

    Create test file

---
+++ test/test.cpp
@@ -0,0 +1,17 @@
+#define CATCH_CONFIG_MAIN
+#include "../src/Menu.hpp"
+#include "../src/TestWord.hpp"
+#include "../src/FirstTest.hpp"
+#include "../src/SecondTest.hpp"
+#include "../src/ThirdTest.hpp"
+#include "../src/Checkword.hpp"
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+/*
+TEST_CASE("check FirstTest", "[func]")
+{
+
+}
+*/
