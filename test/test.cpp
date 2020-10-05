08a3d68faf27c3fce817f29b81cf217c3d5d4e8f
Jeff <XJefferson@rambler.ru>
2020-10-05 20:23:25 +0700

    Update Test function

--- test/test.cpp
+++ test/test.cpp
@@ -5,13 +5,16 @@
 #include "../src/SecondTest.hpp"
 #include "../src/ThirdTest.hpp"
 #include "../src/Checkword.hpp"
+#include "../src/WinLose.hpp"
+#include "../thirdparty/catch.hpp"
 #include <iostream>
 #include <string.h>
 
 using namespace std;
-/*
-TEST_CASE("check FirstTest", "[func]")
+TEST_CASE("check FirstTest", "[WinLose]")
 {
-
+    string V2, V3, List1, List2;
+    int count = 0, win = 0, lose = 0;
+    WinLose(V2,V3,List1,List2,win,lose,count);
+    REQUIRE(win==1)
 }
-*/
