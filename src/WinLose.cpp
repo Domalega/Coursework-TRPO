553595952991c806c3f3977dd264b63c4b1342c7
Domalega <vladimir.domalega@gmail.com>
2020-10-05 19:36:22 +0700

    Fix codestyle

---
+++ src/WinLose.cpp
@@ -0,0 +1,21 @@
+#include <string>
+
+using namespace std;
+
+void WinLose(
+        string* V2,
+        string* V3,
+        string* List1,
+        string* List2,
+        int* win,
+        int* lose,
+        int* count)
+{
+    if (*V2 == *List1 || *V3 == *List2) {
+        *win += 1;
+        *count += 1;
+    } else {
+        *lose += 1;
+        *count += 1;
+    }
+}
