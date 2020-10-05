3a64cbc697f6277d45b3e029b31d30265f428356
MrGanris <ammosov2001@gmail.com>
2020-10-05 12:50:15 +0700

    Revert "Merge branch 'Develop' of https://github.com/Domalega/KursovayaTRPO into Develop"
    This reverts commit f2828995571ba959a6cd11932920dd96bed2497d, reversing
    changes made to 667e9aaa37ff386591bb678b1d111f07392954b5.

--- src/IregV.cpp
+++ src/IregV.cpp
@@ -1,11 +1,11 @@
-#include "Menu.hpp"
 #include <locale.h>
+#include "Menu.hpp"
 
 using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, "Russian");
-    Menu();
-    return 0;
+	setlocale(LC_ALL, "Russian");
+	Menu();
+	return 0;
 }
