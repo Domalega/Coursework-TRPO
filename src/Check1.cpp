ea9e474b7b2e56c2fd7a5efca0fc27e063425e5c
Jeff <XJefferson@rambler.ru>
2020-10-06 21:48:46 +0700

    Create Check1.cpp

---
+++ src/Check1.cpp
@@ -0,0 +1,21 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int Check1(string* V2,
+        string* V3,
+        string* List1,
+        string* List2,
+        int* i);
+{
+    if (Check == ListI[i]) {
+        cout << ListI[i] << " -- " << ListV2[i] << " -- " << ListV3[i] << " -- "
+             << ListT[i] << endl
+             << endl;
+        flag = true;
+        break;
+    }
+    return flag;
+}
