e4b60c6d151f0a1038f112bfe1632dfee54c5a01
Jeff <XJefferson@rambler.ru>
2020-10-06 21:48:55 +0700

    Update test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -6,9 +6,11 @@
 #include "../src/TestWord.hpp"
 #include "../src/ThirdTest.hpp"
 #include "../src/WinLose.hpp"
+#include "../src/Check1.hpp"
 #include "../thirdparty/catch.hpp"
+#include <cstdlib>
 #include <iostream>
-#include <string.h>
+#include <string>
 
 using namespace std;
 TEST_CASE("check WinLose", "[WinLose]")
@@ -19,4 +21,13 @@ TEST_CASE("check WinLose", "[WinLose]")
     WinLose(&V2, &V3, &List1, &List2, &win, &lose, &count);
     REQUIRE(win == 1);
     REQUIRE(lose == 0);
+}
+
+TEST_CASE("check Check1", "[Check1]")
+{
+    string V2 = "1", V3 = "1", List1 = "1", List2 = "1";
+    int i=1;
+
+    Check1(&V2, &V3, &List1, &List2, &i)
+    REQUIRE(flag==true);
 }
